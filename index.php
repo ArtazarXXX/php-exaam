f2b06e211ff02aa3bd87a2c86052e94ec06c1432
ArtazarXXX <artemgribanov886@gmail.com>
2021-01-28 12:11:30 +0300

    add forms + css

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/main_style.css">
+    <link rel="stylesheet" href="/css/main_style.css?<?echo time();?>">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>Start Page</title>
 </head>
@@ -54,7 +54,7 @@
         </nav>
     </header>
     <main>
-        <div class = "add_quiz">
+        <div class="add_quiz">
             <a href = "maker.php">Добавить опрос</a>
         </div>
     </main>
